b0494e27d577173e0829c1383763b05b01fc5be1
pranavrao145 <56097527+pranavrao145@users.noreply.github.com>
2021-05-15 16:04:59 -0400

    first commit

---
+++ SampleRagePlugin/Class1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Windows.Forms;
+using Rage;
+
+[assembly: Rage.Attributes.Plugin("My First Plugin", Description = "This is my first plugin.", Author = "MyName")]
+
+namespace SampleRagePlugin
+{
+    public class EntryPoint
+    {
+        public static void Main()
+        {
+            while (true)
+            {
+                if (Game.IsKeyDown(Keys.NumPad7))
+                {
+                    Game.DisplayNotification("The ~b~key ~w~has been pressed");
+                }
+                
+                GameFiber.Yield();
+            }
+        }
+    }
+}
